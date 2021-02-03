ca0cf7c70d1cbb797a6b330f6c7c441bf02ef6af
perrine1611 <p.toubeau@gmail.com>
2021-02-03 17:07:38 +0100

    first version

---
+++ README.md
@@ -0,0 +1,11 @@
+# README
+
+<h1> Exo 1 : Allociné </h1>
+
+<p>Consigne : Dans cet exercice, on va te faire créer une app Rails dont la BDD contiendra des films. Dans un premier temps, tu vas créer le model et la BDD. Ensuite on va te faire manipuler tout ça avec la console. Pour finir, tu vas seeder ta BDD.</p>
+<br>
+Versions: ruby '2.7.1' avec gem 'rails', '~> 6.1.1' <br>
+<br>
++ utilisation des gem 'table_print' et 'faker'
+
+
---
+++ db/seeds.rb
@@ -0,0 +1,12 @@
+# This file should contain all the record creation needed to seed the database with its default values.
+# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
+#
+# Examples:
+#
+#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
+#   Character.create(name: 'Luke', movie: movies.first)
+
+require 'faker'
+100.times do
+  movie = Movie.create!(name: Faker::Movie.title, year: rand(1900..2020), genre: ["action", "horreur", "comédie", "drame"].sample, synopsis: Faker::Lorem.sentence(word_count: 10) , director: Faker::Name.name , allocine_rating: Faker::Number.within(range: 0.0..5.0).ceil(1) , my_rating: "nil" , already_seen:"false")
+end
